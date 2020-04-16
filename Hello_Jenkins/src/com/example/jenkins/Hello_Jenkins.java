9825758db314e114c22b0199bae91f1bf676750c
sendingt <sendingtotest@gmail.com>
2020-04-16 13:45:26 -0400

    intial commit

---
+++ Hello_Jenkins/src/com/example/jenkins/Hello_Jenkins.java
@@ -0,0 +1,13 @@
+package com.example.jenkins;
+
+public class Hello_Jenkins {
+
+	public static void main(String[] args) {
+
+		// Prints message to the terminal
+
+		System.out.println("Hello from Jenkins");
+
+	}
+
+}
